5268e266b84757527b2e5099bbb7f21f0bbf9915
Carlota Cetina <carlotika26@gmail.com>
2024-04-22 00:13:23 +0200

    Install vuetify and add header and footer design

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="footer-container">
+    <v-footer class="bg-black"> 
+      <v-row justify="center" no-gutters>
+        <v-col class="text-center mt-2" cols="12">
+          <strong>Copyright 2024 — CliniCare</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </div>
+</template>
+  
+
+<style scoped>
+
+  .footer-container{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+ 
+  }
+  
+</style>
+
+
