39f55234d3e73904e308afee1b41ea65d6fa291d
Carlota Cetina <carlotika26@gmail.com>
2024-04-22 11:17:58 +0200

    Add integration with backend to obtain appointment data

---
+++ src/stores/appointmentStore.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import type { Appointment } from '@/core/appointment';
+
+export const useAppointmentStore = defineStore('appointments', () => {
+    const appointments = ref<Appointment[]>([]);
+
+    async function fetchAll() {
+        try {
+            const response = await fetch('https://localhost:7113/Appointments');
+            const appointmentsInfo = await response.json();
+
+            console.log('Data received:', appointmentsInfo);
+
+            appointments.value = appointmentsInfo;
+        } catch (error) {
+            console.error('Error fetching appointment: ', error);
+        }
+    }
+    
+    return {
+        appointments,
+        fetchAll
+    };
+});
